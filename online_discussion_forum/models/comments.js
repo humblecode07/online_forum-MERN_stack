e322599f0d81da36c69b2c148748de6c28410eeb
sakayanagi011 <mikocalderon123@gmail.com>
2024-03-05 15:06:39 -0800

    first commit

---
+++ online_discussion_forum/models/comments.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentsSchema = new Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
+    username: { type: String, required: true },
+    profilePicture: { type: String, required: true },
+    forumPost: { type: mongoose.Schema.Types.ObjectId, ref: 'forums', required: true },
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now},
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    upvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    downvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    replyCount: { type: Number, default: 0 },
+    replies: [{}],
+    edited: { type: Boolean, default: false },
+    editedAt: { type: Date },
+    deleted: { type: Boolean, default: false },
+    deletedAt: { type: Date },
+});
+
+module.exports = mongoose.model("Comments", commentsSchema);
---
+++ online_discussion_forum/models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema({
+    _id: { type: Number, required: true, unique: true },
+    first_name: { type: String, required: true, maxLength: 100},
+    family_name: { type: String, required: true, maxLength: 100},
+    user_name: { type: String, required: true, maxLength: 50 },
+    email: { type: String, required: true, unique: true, match: /^[a-zA-Z0-9._%+-]+@dyci\.edu\.ph$/ },
+    pass: { type: String, required: true, maxLength: 50 },
+    bio: { type: String, maxLength: 100 },
+    date_of_birth: { type: Date },
+    sex: { type: Date },
+    department: { type: String }
+});
+
+module.exports = mongoose.model("Users", accountSchema)
+
